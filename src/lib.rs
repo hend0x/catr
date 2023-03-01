ec6130543f0f04417e67ec179f7b430a966dbb96
hend0x <sinclair500@gmail.com>
2023-03-01 09:42:57 +0000

    Completion of catr implementation, managing the two flags

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,36 @@ pub struct Config {
 
 pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
-        println!("{}", filename);
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num, line) in file.lines().enumerate() {
+                    let line = line?;
+                    if config.number_lines {
+                        println!("{:>6}\t{}", line_num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:>6}\t{}", last_num, line);
+                        } else {
+                            println!("");
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
     }
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -27,25 +54,25 @@ pub fn get_args() -> MyResult<Config> {
         .about("Rust cat")
         .arg(
             Arg::with_name("files")
-            .value_name("FILES")
-            .help("Input file(s)")
-            .multiple(true)
-            .default_value("-"),
-            )
+                .value_name("FILES")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
         .arg(
             Arg::with_name("number")
-            .short("n")
-            .long("number")
-            .help("number lines")
-            .takes_value(false)
-            .conflicts_with("numer_nonblank")
-            )
+                .short("n")
+                .long("number")
+                .help("number lines")
+                .takes_value(false)
+                .conflicts_with("numer_nonblank"),
+        )
         .arg(
             Arg::with_name("number_nonblank")
-            .short("b")
-            .long("number-nonblank")
-            .takes_value(false)
-            )
+                .short("b")
+                .long("number-nonblank")
+                .takes_value(false),
+        )
         .get_matches();
 
     Ok(Config {
